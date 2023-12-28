96a8b386fcfd3e1dd6837f316b7d5194ae3c6fb8
Leo Chuang <chuangl@garena.com>
2023-12-28 16:09:11 +0800

    Add dnd demo

--- src/workshop/FinalSolution.vue
+++ src/workshop/FinalSolution.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref, onMounted } from 'vue'
+import draggable from 'vuedraggable'
 
 import CardPanel from '@/components/Card'
 import CardItem from '@/components/Card/CardItem.vue'
@@ -64,6 +65,16 @@ const deleteItem = (cardId, itemId) => {
     todoList.filter((item) => item.id !== itemId)
   )
 }
+
+const dragOptions = {
+  animation: 200,
+  group: 'description',
+  disabled: false,
+  ghostClass: 'ghost'
+}
+
+const drag = ref(false)
+
 onMounted(() => {
   todoListMap.value.set(defaultCard.id, [defaultItem])
 })
@@ -82,29 +93,50 @@ onMounted(() => {
       />
       <BasicInput v-model="cardTitle" @add:click="createCard(cardTitle)" label="Card Name" />
     </div>
-    <div class="flex flex-wrap content-start">
-      <CardPanel
-        class="mx-2 my-2"
-        v-for="card in cardList"
-        :key="card.id"
-        :color-type="card.colorType"
-        :title="card.title"
-        @title:dblclick="(title) => updateCardTitle(card.id, title)"
-        @delete:click="deleteCard(card.id)"
-        remove-icon
+    <div>
+      <draggable
+        class="flex flex-wrap content-start"
+        :list="cardList"
+        :component-data="{
+          tag: 'ul',
+          type: 'transition-group',
+          name: !drag ? 'flip-list' : null
+        }"
+        @start="drag = true"
+        @end="drag = false"
+        v-bind="dragOptions"
+        itemKey="id"
       >
-        <div class="flex self-end flex-col px-4">
-          <BasicInput v-model="card.todoInput" @add:click="addTodo(card)" />
-
-          <CardItem
-            v-for="item in todoListMap.get(card.id)"
+        <template #item="{ element: card }">
+          <CardPanel
+            class="mx-2 my-2"
+            :key="card.id"
             :color-type="card.colorType"
-            :key="item.id"
-            :item="item.name"
-            @delete:click="deleteItem(card.id, item.id)"
-          />
-        </div>
-      </CardPanel>
+            :title="card.title"
+            @title:dblclick="(title) => updateCardTitle(card.id, title)"
+            @delete:click="deleteCard(card.id)"
+            remove-icon
+          >
+            <div class="flex self-end flex-col px-4">
+              <BasicInput v-model="card.todoInput" @add:click="addTodo(card)" />
+
+              <CardItem
+                v-for="item in todoListMap.get(card.id)"
+                :color-type="card.colorType"
+                :key="item.id"
+                :item="item.name"
+                @delete:click="deleteItem(card.id, item.id)"
+              />
+            </div>
+          </CardPanel>
+        </template>
+      </draggable>
     </div>
   </div>
 </template>
+<style>
+.ghost {
+  opacity: 0.5;
+  background: #c8ebfb;
+}
+</style>
